c9f35837475aad9864178d69882cb58a5ca4cf7a
Fabio Mercado <112907@tecnicatura.frc.utn.edu.ar>
2025-03-25 11:26:44 -0300

    Create index.js

---
+++ exercise_1/index.js
@@ -0,0 +1,7 @@
+/*
+Buscar un Elemento en un Array
+Enunciado:
+    Crea una función buscarElemento(arr, elemento) que devuelva true si el elemento está en el array y false si no.*/
+
+function buscarElemento(arr, elemento) {}
+module.exports = { buscarElemento };
